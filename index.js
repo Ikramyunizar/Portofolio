3bfc916c67ac5b4f2266826130c82578c29122b4
ikram <ikramyunizar@gmail.com>
2020-08-16 11:46:30 +0700

    mini-projects

--- index.js
+++ index.js
@@ -21,3 +21,26 @@ $(function(){
         $(".modal-zoom").attr("src", $(this).find("img").attr('src'))
     })
 })
+
+$(".editmodal").click((e) =>{
+    let idbtn = e.target.id
+    console.log(idbtn);
+    switch (idbtn) {
+        case "covid":
+            $(".modal-title-mini").text("COVID 19 Cases")
+            $(".modal-body-mini").text("A web that gets API from COVID 19 API and display the current info of COVID casualty numbers. Link Below")
+            $(".btn-link").attr('href', "#linkhere" );
+          break;
+        
+        case "mailchimp" :
+            $(".modal-title-mini").text("MailChimp POST API")
+            $(".modal-body-mini").text("A webpage that able to input your email to MailChimp API currently connected to my account. Link Below. ")
+            $(".btn-link").attr('href', "#linkhere" );
+        case "drum" :
+            $(".modal-title-mini").text("drum")
+            $(".modal-body-mini").text("Just like the title says, a drum kit. clickable and keyboard press sensitive. Link Below")
+            $(".btn-link").attr('href', "#linkhere" );
+        default:
+            break;
+    }
+})
