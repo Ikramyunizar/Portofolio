3459e9d243d99d9dca2d0b43aed18bbae87089cb
ikram <ikramyunizar@gmail.com>
2020-08-18 00:00:36 +0700

    layout-fix

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ $(".editmodal").click((e) =>{
     switch (idbtn) {
         case "covid":
             $(".modal-title-mini").text("COVID 19 Cases")
-            $(".modal-body-mini").text("A web that gets API from COVID 19 API and display the current info of COVID casualty numbers. Link Below")
-            $(".btn-link").attr('href', "#linkhere" );
+            $(".modal-body-mini").text("A web that gets API from COVID 19 API and display the current info of COVID casualty numbers. Made with Nodejs, bootstrap, and jquery. Link Below")
+            $(".btn-link").attr('href', "https://covid-cases.glitch.me/" );
           break;
-        
         case "mailchimp" :
             $(".modal-title-mini").text("MailChimp POST API")
-            $(".modal-body-mini").text("A webpage that able to input your email to MailChimp API currently connected to my account. Link Below. ")
-            $(".btn-link").attr('href', "#linkhere" );
-        case "drum" :
-            $(".modal-title-mini").text("drum")
-            $(".modal-body-mini").text("Just like the title says, a drum kit. clickable and keyboard press sensitive. Link Below")
-            $(".btn-link").attr('href', "#linkhere" );
+            $(".modal-body-mini").text("A webpage that able to input your email to MailChimp API currently connected to my account.Made with Nodejs, bootstrap, and jquery Link Below. ")
+            $(".btn-link").attr('href', "https://mailchimp-getter.glitch.me/" );
+            break;
+        case "laravel" :
+            $(".modal-title-mini").text("Laravel CRUD")
+            $(".modal-body-mini").text("A Simple CRUD app made with Laravel and pagination" )
+            $(".btn-link").attr('href', "https://nutechikram.000webhostapp.com" );
         default:
             break;
     }
